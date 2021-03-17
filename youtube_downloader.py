95d5bd7c4042f417a94cd07d46d6edd0944d2861
Timo Unger <dodo92@gmx.de>
2021-03-17 15:08:10 +0100

    gui

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -7,52 +7,39 @@
 *****************************************************************************
 """
 
-import sys
 import subprocess
 import pytube
+from tkinter import *
 
 S_VERSION = "0.1"
+S_TITEL = "YouTube Downloader"
 S_DOWNLOAD_FOLDER = "Download"
 
-def download(s_url, s_format):
+b_button_mp4_pressed = False
+
+def download():
     """ download YouTube content and open explorer if successful """
+    notif.config(fg="green",text="Download wird durchgeführt...")
     try:
-        print("Download wird durchgeführt...")
-        yt_obj = pytube.YouTube(s_url)
+        yt_obj = pytube.YouTube(s_url.get())
         filters = yt_obj.streams.filter(progressive=True, file_extension="mp4")
         # download_loop the highest quality video
         filters.get_highest_resolution().download(S_DOWNLOAD_FOLDER)
-        print('Download war erfolgreich!')
+        notif.config(fg="green",text="Download war erfolgreich!")
         subprocess.Popen('explorer ' + S_DOWNLOAD_FOLDER)
-    except Exception as s_error: # pylint: disable=broad-except 
-        print(s_error)
-        print('Download war nicht erfolgreich!')
-
-def user_input(s_text):
-    print(s_text)
-    s_input = input(">> ")
-    return s_input
-
-def download_loop():
-    """ download loop for YouTube content """
-    print("Youtube Downloader V%s\n" % S_VERSION)
-    b_run = True
-    while b_run:
-        s_url = user_input("Gebe die YouTube URL ein:")
-        """
-        s_format_input = user_input("Drücke Enter für ein mp4 Format, gebe 0 ein für eine mp3:")
-        if s_format_input == "0":
-            s_format = "mp4"
-        else:
-            s_format = "mp3"
-        """
-        download(s_url, s_format)
-        s_input = user_input("Drücke Enter um das Programm zu beenden, gebe 0 ein für einen weiteren Download.")
-        if s_input == "0":
-            b_run = True
-        else:
-            b_run = False
+    except Exception: # pylint: disable=broad-except 
+        notif.config(fg="red",text="Download war nicht erfolgreich!")
 
 if __name__ == "__main__":
-    download_loop()
-    sys.exit("PROGRAM END")
+    master = Tk()
+    master.title(S_TITEL + " V%s\n" % S_VERSION)
+    Label(master, text=S_TITEL,fg="red",font=("Calibri",15)).grid(sticky=N,padx=100,row=0)
+    Label(master,text="Gebe die YouTube URL ein:",font=("Calibri",12)).grid(sticky=N,row=1,pady=15)
+    Label(master, text="Timo Unger © 2021",fg="black",font=("Calibri",12)).grid(sticky=N,padx=100,row=6)
+    notif = Label(master,font=("Calibri",12))
+    notif.grid(sticky=N,pady=1,row=5)
+    s_url=StringVar()
+    Entry(master,width=50,textvariable=s_url).grid(sticky=N,row=2)
+    Button(master,width=20,text="Download .mp4",font=("Calibri",12),command=download).grid(sticky=N,row=3,pady=15) #Button mp4
+    Button(master,width=20,text="Download .mp3",font=("Calibri",12),command=download).grid(sticky=N,row=4,pady=15) #Button mp3
+    master.mainloop()
