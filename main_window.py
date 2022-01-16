4a86d2b7b9604369e89117fa6ae083a92661d9f6
Timo <dodo92@gmx.de>
2022-01-16 13:24:46 +0100

    queue start

--- main_window.py
+++ main_window.py
@@ -15,8 +15,11 @@ import threading
 import time
 import clipboard
 import youtube_download as yd
+from youtube_download import EState
 
 L_ROW_DESCRIPTION = ["Titel", "Length", "Format", "Status", "ID"]
+
+I_STATUS_POS = 3
 
 class CGui(object):
     def __init__(self, window, gui):
@@ -37,20 +40,23 @@ class CGui(object):
         self.c_queue = CQueue(gui)
         self.c_queue.start()
         self.gui.text_url.setPlainText("https://www.youtube.com/watch?v=q5WdWSpz1vQ")
-        self.gui.text_url.setPlainText("https://www.youtube.com/watch?v=K6lThwljr2M&list=PL9HJJedWjAmNILSM93NSf0-5-VoLXs_6Q&index=2")
+        self.gui.text_url.setPlainText("https://www.youtube.com/watch?v=jfotDk-fTOk&list=PLZq2gDq-SDjf5F35rolsQ3eFPvfTUKYpC")
     def add_clicked(self):
         self.gui.statusbar.showMessage("URL wird analysiert...")
         s_input = self.gui.text_url.toPlainText()
         s_format = self.gui.combo_format.currentText()
         self.gui.text_url.clear()
-        self.c_queue.l_queue += yd.analysis_url(s_input, s_format)
-        self.gui.statusbar.showMessage("URL wurde analysiert!")
+        c_analyse = yd.CAnalyzeUrl(self.c_queue.l_queue, s_input, s_format)
+        c_analyse.start()
     def paste_clicked(self):
         """ input text from clipboard to entry box """
         self.gui.text_url.setPlainText(clipboard.paste())
         self.gui.statusbar.showMessage("Text aus Zwischenablage wurde eingefügt!")
     def download_clicked(self):
         self.gui.statusbar.showMessage("Videos werden heruntergeladen...")
+        for entry in self.c_queue.l_queue:
+            if entry[I_STATUS_POS] == EState.ANALYSED:
+                entry[I_STATUS_POS] = EState.QUEUED
 
 class CQueue(threading.Thread):
     """ thread class for queue """
@@ -58,46 +64,68 @@ class CQueue(threading.Thread):
         threading.Thread.__init__(self)
         self.gui = gui
         self.l_queue = []
+        self.c_download = None
     def run(self):
         time.sleep(0.001)
         while True:
             time.sleep(0.1)
+            self.process_queue()
             self.set_status_table()
-    def set_status_table(self):
-        pass
-        """
-        table = self.gui.table_queue
-        row_cnt = table.rowCount()
-        i_id_pos = 4
-        i_format_pos = 2
-        b_removed = False
-        for i_row in range(0, row_cnt):
-            b_exist = False
-            for entry in self.l_queue:
-                if (table.item(i_row, i_id_pos).text() == entry[i_id_pos])\
-                   and (table.item(i_row, i_format_pos).text() == entry[i_format_pos]):
-                    b_exist = True
+    def process_queue(self):
+        for i, entry in enumerate(self.l_queue):
+            if entry[I_STATUS_POS] == EState.QUEUED:
+                b_download = False
+                if self.c_download is None:
+                    b_download = True
+                if self.c_download != None:
+                    if self.c_download.entry[I_STATUS_POS] == EState.FINISH:
+                        b_download = True
+                if b_download:
+                    entry[I_STATUS_POS] = EState.DOWNLOAD
+                    self.c_download = yd.CDownload(entry)
+                    self.c_download.start()
                     break
-            if not b_exist:
-                table.removeRow(i_row)
-                b_removed = True
-                break
-        if not b_removed:
-            for entry in self.l_queue:
-                b_entry_exist = False
-                row_cnt = table.rowCount()
-                i_update_pos = row_cnt
-                for i_row in range(0, row_cnt):
+    def set_status_table(self):
+        if False:
+            table = self.gui.table_queue
+            table.setRowCount(0)
+            for i, entry in enumerate(self.l_queue):
+                table.insertRow(i)
+                for j, _ in enumerate(L_ROW_DESCRIPTION):
+                    table.setItem(i, j, QTableWidgetItem(entry[j]))
+        else:
+            table = self.gui.table_queue
+            row_cnt = table.rowCount()
+            i_id_pos = 4
+            i_format_pos = 2
+            b_removed = False
+            for i_row in range(0, row_cnt):
+                b_exist = False
+                for entry in self.l_queue:
                     if (table.item(i_row, i_id_pos).text() == entry[i_id_pos])\
                        and (table.item(i_row, i_format_pos).text() == entry[i_format_pos]):
-                        b_entry_exist = True
-                        i_update_pos = i_row
+                        b_exist = True
                         break
-                if not b_entry_exist:
-                    table.insertRow(row_cnt)
-                for i, _ in enumerate(L_ROW_DESCRIPTION):
-                    table.setItem(i_update_pos, i, QTableWidgetItem(entry[i]))
-    """
+                if not b_exist:
+                    table.removeRow(i_row)
+                    b_removed = True
+                    break
+            if not b_removed:
+                for entry in self.l_queue:
+                    b_entry_exist = False
+                    row_cnt = table.rowCount()
+                    i_update_pos = row_cnt
+                    for i_row in range(0, row_cnt):
+                        if (table.item(i_row, i_id_pos).text() == entry[i_id_pos])\
+                           and (table.item(i_row, i_format_pos).text() == entry[i_format_pos]):
+                            b_entry_exist = True
+                            i_update_pos = i_row
+                            break
+                    if not b_entry_exist:
+                        table.insertRow(row_cnt)
+                    for i, _ in enumerate(L_ROW_DESCRIPTION):
+                        table.setItem(i_update_pos, i, QTableWidgetItem(entry[i]))
+
 if __name__ == "__main__":
     o_app = QApplication(sys.argv)
     o_window = QMainWindow()
--- youtube_download.py
+++ youtube_download.py
@@ -13,6 +13,7 @@ import subprocess
 import time
 import re
 import pytube
+import threading
 from moviepy.editor import AudioFileClip
 
 S_DEBUG = False
@@ -25,36 +26,59 @@ L_FORMAT = [
     ("Nur Audio",3)
 ]
 
+class EState():
+    ANALYSED = "Analyzed"
+    QUEUED = "Queued"
+    DOWNLOAD = "Download"
+    FINISH = "Finish"
+
 S_VIDEO_URL = "https://www.youtube.com/watch?v=%d" # q5WdWSpz1vQ
         
 def progress_callback(self, _stream, _chunk, bytes_remaining):
     """ calculate process and update process bar """
     pass
 
-def analysis_url(s_text, i_choice):
-    """ analysis URL """
-    l_video_list = []
-    s_text = s_text.splitlines()
-    for s_url_text in s_text:
-        l_url = []
-        s_subfolder_name = None
-        if re.search("&list=", s_url_text):
-            o_playlist = pytube.Playlist(s_url_text)
-            s_subfolder_name = o_playlist.title
-            for video in o_playlist.videos:
-                l_url.append(video.watch_url)
-        else:
-            l_url = [s_url_text]
-        for s_url in l_url:
-            try:
-                o_youtube = pytube.YouTube(s_url, on_progress_callback=progress_callback)
-                s_titel = o_youtube.title
-                s_length = time.strftime("%H:%M:%S", time.gmtime(o_youtube.length))
-                s_id = o_youtube.video_id
-                l_video_list.append([s_titel, s_length, i_choice, "Ready", s_id, s_subfolder_name, o_youtube])
-            except: # pylint: disable=bare-except
-                pass
-    return l_video_list
+class CAnalyzeUrl(threading.Thread):
+    """ thread class for download """
+    def __init__(self, l_queue, s_input, s_format):
+        self.l_queue = l_queue
+        self.s_input = s_input
+        self.s_format = s_format
+        threading.Thread.__init__(self)
+    def run(self):
+        """ analysis URL """
+        s_input = self.s_input.splitlines()
+        for s_url_text in s_input:
+            l_url = []
+            s_subfolder_name = None
+            if re.search("&list=", s_url_text):
+                o_playlist = pytube.Playlist(s_url_text)
+                s_subfolder_name = o_playlist.title
+                for video in o_playlist.videos:
+                    l_url.append(video.watch_url)
+            else:
+                l_url = [s_url_text]
+            for s_url in l_url:
+                try:
+                    o_youtube = pytube.YouTube(s_url)
+                    s_titel = o_youtube.title
+                    s_length = time.strftime("%H:%M:%S", time.gmtime(o_youtube.length))
+                    s_id = o_youtube.video_id
+                    l_entry = [s_titel, s_length, self.s_format, EState.ANALYSED, s_id, s_subfolder_name, o_youtube]
+                    self.l_queue.append(l_entry)
+                except: # pylint: disable=bare-except
+                    pass
+
+class CDownload(threading.Thread):
+    """ thread class for download """
+    def __init__(self, entry):
+        threading.Thread.__init__(self)
+        self.entry = entry
+    def run(self):
+        o_stream = self.entry[6].streams.filter(progressive=True, file_extension='mp4')\
+                                             .get_highest_resolution()
+        o_stream.download(S_DOWNLOAD_FOLDER)
+        self.entry[3] = EState.FINISH
 
 def open_download_folder():
     """ open download folder and create if not exist """
