239d4a5600b659e13402d466930791068336a6b6
Timo Unger <dodo92@gmx.de>
2023-06-17 09:39:41 +0200

    [CI] PyLint fix

--- Test/run_pylint.bat
+++ Test/run_pylint.bat
@@ -3,14 +3,15 @@
 
 @echo off
 
-set MAIN_DIR=../
+set MAIN_DIR=..\
 set TEST_DIR=Test
 set TARGET_DIR=Source
 set IGNORE_LIST=
 set CONFIG_FILE=%TEST_DIR%\.pylintrc
-set LOG_FILE=%TEST_DIR%/pylint_YouTubeDownloader.log
+set LOG_FILE=%TEST_DIR%\pylint_YouTubeDownloader.log
 
 cd %MAIN_DIR%
-pylint --rcfile=%CONFIG_FILE% %TARGET_DIR% --reports=y --output=%LOG_FILE% --ignore=%IGNORE_LIST%
+pylint --rcfile=%CONFIG_FILE% %TARGET_DIR% --reports=y --output=%LOG_FILE% --output-format=text --ignore=%IGNORE_LIST%
+type %LOG_FILE%
 echo 'pylint exit code: %ERRORLEVEL%'>>%LOG_FILE%
-notepad %LOG_FILE%
+start notepad %LOG_FILE%
