01b3c1186ba658507c43abddc168d61187b8e618
Timo Unger <dodo92@gmx.de>
2023-06-17 23:02:51 +0200

    a

--- Test/run_pylint.bat
+++ Test/run_pylint.bat
@@ -12,8 +12,8 @@ set LOG_FILE=%TEST_DIR%\pylint_YouTubeDownloader.log
 
 cd %MAIN_DIR%
 pylint --rcfile=%CONFIG_FILE% %TARGET_DIR% --reports=y --output=%LOG_FILE% --output-format=text --ignore=%IGNORE_LIST%
-::type %LOG_FILE%
-::echo 'pylint exit code: %ERRORLEVEL%'>>%LOG_FILE%
+type %LOG_FILE%
+echo 'pylint exit code: %ERRORLEVEL%'>>%LOG_FILE%
 ::start notepad %LOG_FILE%
 if errorlevel 1 (
   echo Pylint failed with error code %errorlevel%.
